fa1ba40b050929f1854c10b5b73f90ec00fc39b8
shtora123 <shtora123@local>
2024-01-04 20:24:06 +0200

    functions

--- games/words/game.js
+++ games/words/game.js
@@ -57,45 +57,65 @@ var words = [
     "package",
     "function",
 ];
-
-var word = words[Math.floor(Math.random()*words.length)]
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
+function pickWord() {
+    return words[Math.floor(Math.random()*words.length)]
 }
+function setupAnswerArray(word) {
+    var answerArray = []
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    }
+    return answerArray;
+}
+function showPlayerProgress(answerArray) {
+    const task = answerArray.join(" ")
+    return guess = prompt(`${task}\n\n👆🏻\nGuess the letter of the word, or if you want to exit, press the <Cancel> button`)
+}
+function updateGameState(guess, word, answerArray) {
+    var numberOfCorrectAnwsers = 0
+    for (var j = 0; j < word.length; j++) { 
+        if (word[j].toLowerCase() === guess.toLowerCase() && answerArray[j] === "_") {
+            answerArray[j] = word[j]; 
+            numberOfCorrectAnwsers += 1
+        }
+    }
+    return numberOfCorrectAnwsers;
+}
+function showAnswerAndCongratulatePlayer(word,remainingLetters) {
+    if (remainingLetters === 0){
+        alert(`Well done, you won. The word was spoken "${word}"\n🎉🎉🎉`)
+    }
+};
+
+
+
+var word = pickWord()
+
+var answerArray = setupAnswerArray(word);
 var remainingLetters = word.length;
-var wrongAnswer = word.length
-
+var wrongAnswer = word.length + 1
+ 
 while (remainingLetters > 0) {
-
-        const task = answerArray.join(" ")
-        var guess = prompt(`${task}\n\n👆🏻\nGuess the letter of the word, or if you want to exit, press the <Cancel> button`)
-        if  (guess === null){
-            alert('ok, bye!👋🏻')
-            break;
-        } else if (guess.length !== 1){
+    showPlayerProgress(answerArray)
+    if  (guess === null){
+        alert('ok, bye!👋🏻')
+        break;
+    } else if (guess.length !== 1){
             alert("Please enter only one letter.")
 
         }else{
-            var userGuessed = false          
-            
-            for (var j = 0; j < word.length; j++) { 
-                    if (word[j].toLowerCase() === guess.toLowerCase() && answerArray[j] === "_") {
-                        answerArray[j] = word[j]; 
-                        remainingLetters--; 
-                        userGuessed = true;
-                    }
-            }
-            if (!userGuessed){
+            const numberOfCorrectAnwsers = updateGameState(guess, word, answerArray)
+            remainingLetters -= numberOfCorrectAnwsers;
+
+            if (!numberOfCorrectAnwsers){
                 wrongAnswer --
             }
             if (wrongAnswer === 0){
                 alert('so many wrong anwsers')
                 break;
             }
+ 
         }
 }
 
-if (remainingLetters === 0){
-    alert(`Well done, you won. The word was spoken "${word}"\n🎉🎉🎉`)
-}
+showAnswerAndCongratulatePlayer(word, remainingLetters);
